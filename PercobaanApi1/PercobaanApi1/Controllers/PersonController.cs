66896672b4c2502b820094277e6e441e6ea471d0
Fadhila Fathin A.Z <117797170+fadhilafathin1@users.noreply.github.com>
2024-03-04 23:14:07 +0700

    Add files via upload

---
+++ PercobaanApi1/PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            IsiPerson context = new IsiPerson();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+        [HttpGet("api/person/{id}")]
+        public ActionResult<Person> GetPerson(int id)
+        {
+            IsiPerson context = new IsiPerson();
+            var person = context.ListPerson().FirstOrDefault(p => p.id_person == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+    }
+}
